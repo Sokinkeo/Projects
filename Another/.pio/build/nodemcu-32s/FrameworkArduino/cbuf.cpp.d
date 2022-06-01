10132294d0bf6788473c9507e805c7dd71d64fe3
Sokinkeo <73463883+Sokinkeo@users.noreply.github.com>
2022-06-01 22:19:10 +0700

    Newest
    Hi

---
+++ Another/.pio/build/nodemcu-32s/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/nodemcu-32s/FrameworkArduino/cbuf.cpp.o: \
+ /Users/SokinKeo/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/SokinKeo/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ Another/.pio/build/nodemcu-32s/FrameworkArduino/esp32-hal-tinyusb.c.d
@@ -0,0 +1,3 @@
+.pio/build/nodemcu-32s/FrameworkArduino/esp32-hal-tinyusb.c.o: \
+ /Users/SokinKeo/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
+ /Users/SokinKeo/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qspi_qspi/include/sdkconfig.h
