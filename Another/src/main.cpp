bfcc0c66cf3a26c24a8d220f2f4d5dfb9e4b306c
Sokinkeo <73463883+Sokinkeo@users.noreply.github.com>
2022-06-04 15:37:58 +0700

    Send data to google sheet

--- Another/src/main.cpp
+++ Another/src/main.cpp
@@ -1,26 +1,67 @@
-#include <Arduino.h>
-#include <WiFi.h>
-
-const char* ssid = "King";
-const char* password = "12345678a";
+//Include required libraries
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include "time.h"
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 19800;
+const int   daylightOffset_sec = 0;
+// WiFi credentials
+const char* ssid = "King";         // change SSID
+const char* password = "12345678a";    // change password
+// Google script ID and required credentials
+String GOOGLE_SCRIPT_ID = "AKfycbxJTfECp84D2AzWmmcsx6aJQIJRGWnB1V8guXlDqZFsqbge-L8";    // change Gscript ID
+//https://script.google.com/macros/s/AKfycbxJTfECp84D2AzWmmcsx6aJQIJRGWnB1V8guXlDqZFsqbge-L8/exec
+int count = 0;
 void setup() {
-    Serial.begin(9600);
-    delay(1000);
-
-    WiFi.mode(WIFI_STA); //Optional
-    WiFi.begin(ssid, password);
-    Serial.println("\nConnecting");
-
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-    }
-
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+  delay(1000);
+  Serial.begin(9600);
+  delay(1000);
+  // connect to WiFi
+  Serial.println();
+  Serial.print("Connecting to wifi: ");
+  Serial.println(ssid);
+  Serial.flush();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
-
 void loop() {
-
-}
+   if (WiFi.status() == WL_CONNECTED) {
+    static bool flag = false;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+      Serial.println("Failed to obtain time");
+      return;
+    }
+    char timeStringBuff[50]; //50 chars should be enough
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+    String asString(timeStringBuff);
+    asString.replace(" ", "-");
+    Serial.print("Time:");
+    Serial.println(asString);
+    String urlFinal = "https://script.google.com/macros/s/"+GOOGLE_SCRIPT_ID+"/exec?"+"date=" + asString + "&sensor=" + String(count);
+    Serial.print("POST data to spreadsheet:");
+    Serial.println(urlFinal);
+    HTTPClient http;
+    http.begin(urlFinal.c_str());
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    int httpCode = http.GET(); 
+    Serial.print("HTTP Status Code: ");
+    Serial.println(httpCode);
+    //---------------------------------------------------------------------
+    //getting response from google sheet
+    String payload;
+    if (httpCode > 0) {
+        payload = http.getString();
+        Serial.println("Payload: "+payload);    
+    }
+    //---------------------------------------------------------------------
+    http.end();
+  }
+  count++;
+  delay(1000);
+} 
